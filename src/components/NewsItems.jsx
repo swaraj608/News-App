72b3d480b8a4be8808aa5c66d69f1999ea2e2a61
swaraj608 <singhaniajahnvi260@gmail.com>
2025-03-08 22:57:32 +0530

    Clean up comments in codebase

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,18 +1,4 @@
-// import image from '../assets/bn.jpeg'
-// const NewsItems = ({ title, description, src, url }) => {
-//   return (
-//     <div className="card" style={{ width: '18rem' }}>
-//       <img src={src ? src : bn} className="card-img-top" alt="..." />
-//       <div className="card-body">
-//         <h5 className="card-title">{title}</h5>
-//         <p className="card-text">{description}</p>
-//         <a href={url} className="btn btn-primary">Read More</a>
-//       </div>
-//     </div>
-//   );
-// };
 
-// export default NewsItems;
 
 import bn from '../assets/bn.jpeg'; 
 
